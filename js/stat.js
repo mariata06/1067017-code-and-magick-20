ab6c2f1c5fde19bf6831e1e5b08cb1e8ad288d73
Мария <slinkywizard@mail.ru>
2020-06-02 16:13:14 +0300

    Внесение изменений: добавила функцию renderTitle, Math.random для цвета колоно к, тернарный оператор вместо if, удалила у текста лишний fillStyle

--- js/stat.js
+++ js/stat.js
@@ -25,31 +25,25 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var renderTitle = function(ctx, string1, string2) {
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText(string1, CLOUD_X + GAP / 2, GAP);
+  ctx.fillText(string2, CLOUD_X + GAP / 2, GAP * 1.5);
+}
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP / 4, CLOUD_Y + GAP / 4, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  var color = ['hsl(230, 5%, 25%)', 'hsl(230, 60%, 25%)', 'hsl(230, 100%, 25%)', 'hsl(230, 32%, 25%)'];
+  renderTitle(ctx,'Ура вы победили!', 'Список результатов:');
 
   var maxTime = getMaxElement(times);
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP / 2, GAP);
-  ctx.fillStyle = '#000';
-  ctx.fillText('Список результатов:', CLOUD_X + GAP / 2, GAP * 1.5);
-
-
   for (var i = 0; i < players.length; i++) {
     ctx.font = '16px PT Mono';
-    ctx.fillStyle = '#000';
     ctx.fillText(Math.round(times[i]), CLOUD_X + GAP / 4 + BAR_WIDTH + (BAR_WIDTH + GAP) * i, CLOUD_HEIGHT + GAP / 2 - (GAP + (MAX_BAR_HEIGHT * times[i]) / maxTime));
 
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = color[i];
-    }
+    players[i] === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'hsl(230,' + Math.random() * 100 + '%, 25%)';
 
     ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP / 3, BAR_WIDTH, -((MAX_BAR_HEIGHT * times[i]) / maxTime));
 
