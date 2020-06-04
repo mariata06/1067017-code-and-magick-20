7e91c7eaa07f4d625a9620af1e77334b3aa4a2cd
Мария <slinkywizard@mail.ru>
2020-06-05 00:32:52 +0300

    Учебный проект: нас орда

---
+++ js/setup.js
@@ -0,0 +1,43 @@
+var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR =['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template');
+var newWizard = similarWizardTemplate.content.querySelector('.setup-similar-item');
+
+var randomElement = function(array) {
+  return array[Math.floor(array.length * Math.random())];
+}
+
+var wizards = [];
+
+for (var i = 0; i < 4; i++) {
+  var wiz = {
+    name: randomElement(WIZARDS_NAMES) + ' ' + randomElement(WIZARDS_SURNAMES),
+    coatColor: randomElement(COAT_COLOR),
+    eyesColor: randomElement(EYES_COLOR)
+  }
+  wizards.push(wiz);
+}
+
+var renderWizard = function(wizard) {
+  var wizardElement = newWizard.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+}
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
